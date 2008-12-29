82f451714a62caac143fd75e688b7a861543939e
Teflon Ted <github@rudiment.net>
2008-12-28 22:01:28 -0500

    added missing email column to users table; all tests passing

--- db/migrate/20081229015501_create_users.rb
+++ db/migrate/20081229015501_create_users.rb
@@ -3,6 +3,7 @@ class CreateUsers < ActiveRecord::Migration
     create_table :users do |t|
       t.timestamps
       t.string :login, :null => false
+      t.string :email, :null => false
       t.string :crypted_password, :null => false
       t.string :password_salt, :null => false
       t.string :persistence_token, :null => false
