c1a75b5ba272b9e665fb9d0155dac5f615db3e05
Teflon Ted <github@rudiment.net>
2008-12-28 21:21:57 -0500

    trying to get authlogic working; the documentation is sparse regarding the necessary models, views, and controllers

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -7,9 +7,22 @@ class ApplicationController < ActionController::Base
   # See ActionController::RequestForgeryProtection for details
   # Uncomment the :secret if you're not using the cookie session store
   protect_from_forgery # :secret => 'd82fa1b6d12b49870facf1598f5047c9'
-  
+
   # See ActionController::Base for details 
   # Uncomment this to filter the contents of submitted sensitive data parameters
   # from your application log (in this case, all fields with names like "password"). 
   # filter_parameter_logging :password
+
+  helper_method :current_user_session, :current_user
+
+  private
+    def current_user_session
+      return @current_user_session if defined?(@current_user_session)
+      @current_user_session = UserSession.find
+    end
+
+    def current_user
+      return @current_user if defined?(@current_user)
+      @current_user = current_user_session && current_user_session.user
+    end
 end
