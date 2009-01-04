cd57d45ddddd1f2ef0fdc7710e935492476317b8
Teflon Ted <github@rudiment.net>
2009-01-04 13:42:35 -0500

    removed unused views and vendor'd haml gem

--- config/environment.rb
+++ config/environment.rb
@@ -36,6 +36,7 @@ Rails::Initializer.run do |config|
   config.gem 'rubyist-aasm', :lib => 'aasm', :source => 'http://gems.github.com', :version => '2.0.2'
   config.gem 'mislav-will_paginate', :version => '2.3.6', :lib => 'will_paginate', :source => 'http://gems.github.com'
   config.gem 'youtube-g', :lib => 'youtube_g', :version => '0.4.9.9'
+  config.gem 'haml', :version => '2.0.6'
   
   # These cause problems with irb. Left in for reference
   # config.gem 'rspec-rails', :lib => 'spec/rails', :version => '1.1.11'
