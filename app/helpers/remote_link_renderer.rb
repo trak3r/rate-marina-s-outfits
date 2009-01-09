2ba61e6f909ff8b7f2d6f1c0a2954d6880f41057
Teflon Ted <github@rudiment.net>
2009-01-08 19:31:22 -0500

    fixed ajaxy pagination snafu

--- app/helpers/remote_link_renderer.rb
+++ app/helpers/remote_link_renderer.rb
@@ -3,11 +3,12 @@
 class RemoteLinkRenderer < WillPaginate::LinkRenderer
   def prepare(collection, options, template)
     @remote = options.delete(:remote) || {}
+    @url_overrides = @remote.delete(:url)
     super
   end
 
 protected
   def page_link(page, text, attributes = {})
-    @template.link_to_remote(text, {:url => url_for(page), :method => :get}.merge(@remote))
+    @template.link_to_remote(text, {:url => {:page => page}.merge(@url_overrides), :method => :get}.merge(@remote))
   end
 end
